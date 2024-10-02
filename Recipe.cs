c82c5fc4d987e86840f5844bcc15760ad930ff31
schja278 <schja278@student.otago.ac.nz>
2024-10-02 20:36:33 +1300

    removed recipe changes because turns out they already existed

--- Recipe.cs
+++ Recipe.cs
@@ -18,11 +18,7 @@ namespace TheBereftSouls
     {
         public override void AddRecipeGroups()
         {
-            RecipeGroup group = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.MagicMirror)}", ItemID.IceMirror, ItemID.MagicMirror);
-            RecipeGroup.RegisterGroup(nameof(ItemID.MagicMirror), group);
             
-            group = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} {Lang.GetItemNameValue(ItemID.RottenChunk)}", ModContent.ItemType<BloodSample>(), ItemID.RottenChunk);
-            RecipeGroup.RegisterGroup(nameof(ItemID.RottenChunk), group);
         }
         public override void AddRecipes()
         {
@@ -33,46 +29,7 @@ namespace TheBereftSouls
 
         public override void PostAddRecipes()
         {
-            Main.recipe.Where(x => x.createItem.type == ModContent.ItemType<StormFlare>()).ToList().ForEach(s =>
-            {
-                //s.DisableRecipe(); //disables the recipe
-                s.RemoveIngredient(ModContent.ItemType<Talon>()); //remove ingredient
-                s.AddIngredient(ModContent.ItemType<StormlionMandible>()); //add ingredient
-            });
-            Main.recipe.Where(x => x.createItem.type == ModContent.ItemType<GrandFlareGun>()).ToList().ForEach(s =>
-            {
-                //s.RemoveIngredient(ModContent.ItemType<Talon>()); // kept talon in the recipe because I believe they should still be used for at least the flare gun(this can be changed)
-                s.AddIngredient(ModContent.ItemType<StormlionMandible>());
-            });
-
-            Main.recipe.Where(x => x.createItem.type == ModContent.ItemType<DesertMedallion>()).ToList().ForEach(s =>
-            {
-                s.AddIngredient(ModContent.ItemType<SandstoneIngot>(), 5);//boss progression document said too add storm feathers to the recipe but I have no clue what those are (custom item?) added GTB sandstone ingot drop instead
-            });
-
-            Main.recipe.Where(x => x.createItem.type == ModContent.ItemType<DecapoditaSprout>()).ToList().ForEach(s =>
-            {
-                s.AddIngredient(ModContent.ItemType<SandstoneIngot>(), 5);
-                s.AddIngredient(ModContent.ItemType<PearlShard>(), 2);
-            });
-
-            Main.recipe.Where(x => x.createItem.type == ModContent.ItemType<UnstableCore>()).ToList().ForEach(s =>
-            {
-                s.AddRecipeGroup(nameof(ItemID.RottenChunk), 5);
-                //s.AddIngredient(ModContent.ItemType<DraculaFang>(), 2); should probably add a custom item instead of this as its only a 14% drop chance
-            });
-
-            Main.recipe.Where(x => x.createItem.type == ModContent.ItemType<AncientBlade>()).ToList().ForEach(s =>
-            {
-                s.AddRecipeGroup(nameof(ItemID.RottenChunk), 5);
-                //s.AddIngredient(ModContent.ItemType<DraculaFang>(), 2); should probably add a custom item instead of this as its only a 14% drop chance
-            });
-
-            /*Main.recipe.Where(x => x.createItem.type == ModContent.ItemType<OverloadedSludge>()).ToList().ForEach(s =>
-            {
-                s.AddIngredient(ModContent.ItemType<marble item>(), 5);
-                s.AddIngredient(ModContent.ItemType<granite item>(), 5);
-            });*/
+            
         }
     }
 }
