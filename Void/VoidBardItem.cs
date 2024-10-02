34a8e2736349e0846e96eb64abb5b0aab390c531
schja278 <schja278@student.otago.ac.nz>
2024-10-02 15:51:41 +1300

    why is everything sealed :(

--- Void/VoidBardItem.cs
+++ Void/VoidBardItem.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria.ModLoader;
 using ThoriumMod.Items;
 
 namespace TheBereftSouls.Void
 {
-    public abstract class VoidBardItem : ModItem
-    {
-    }
+    //public abstract class VoidBardItem : ModItem
+    //{
+    //}
 }
