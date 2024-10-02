0f3d3eba2f982d439b6a818f377a5dfaf69c5aa3
schja278 <schja278@student.otago.ac.nz>
2024-10-02 23:30:31 +1300

    added stealth strike to gold chakram

--- Items/Weapons/Rogue/Void/GoldChakramOverride.cs
+++ Items/Weapons/Rogue/Void/GoldChakramOverride.cs
@@ -5,6 +5,8 @@ using TheBereftSouls.Void;
 using SOTS;
 using TheBereftSouls.Projectiles.Rogue.Void;
 using CalamityMod;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
 
 namespace TheBereftSouls.Items.Weapons.Rogue.Void
 {
@@ -41,6 +43,23 @@ namespace TheBereftSouls.Items.Weapons.Rogue.Void
         public override void AddRecipes()
         {
             CreateRecipe(1).AddIngredient(ItemID.GoldBar, 15).AddTile(TileID.Anvils).Register();
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (player.Calamity().StealthStrikeAvailable())
+            {
+                int spread = 10;
+                for (int i = 0; i < 3; i++)
+                {
+                    Vector2 perturbedspeed = velocity.RotatedBy(MathHelper.ToRadians(spread));
+                    int p = Projectile.NewProjectile(source, position.X, position.Y, perturbedspeed.X, perturbedspeed.Y, type, damage*2, knockback, player.whoAmI, 0f, 1f);
+                    if (p.WithinBounds(Main.maxProjectiles))
+                        Main.projectile[p].Calamity().stealthStrike = true;
+                    spread -= 10;
+                }
+                return false;
+            }
+            return true;
         }
     }
 }
