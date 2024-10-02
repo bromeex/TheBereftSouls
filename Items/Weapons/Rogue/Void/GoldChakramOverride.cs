97ec8a0293bc5318be051517529193bacae1d1b9
schja278 <schja278@student.otago.ac.nz>
2024-10-02 22:19:28 +1300

    functioning gold chakram replacement

---
+++ Items/Weapons/Rogue/Void/GoldChakramOverride.cs
@@ -0,0 +1,46 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using TheBereftSouls.Void;
+using SOTS;
+using TheBereftSouls.Projectiles.Rogue.Void;
+using CalamityMod;
+
+namespace TheBereftSouls.Items.Weapons.Rogue.Void
+{
+    public class GoldChakramOverride : VoidDamageItem
+    {
+        public override string Texture => "SOTS/Items/OreItems/GoldChakram";
+        public override void SetStaticDefaults()
+        {
+            this.SetResearchCost(1);
+        }
+        public override void SafeSetDefaults()
+        {
+            Item.damage = 17;
+            Item.DamageType = ModContent.GetInstance<RogueDamageClass>();
+            Item.width = 30;
+            Item.height = 34;
+            Item.useTime = 36;
+            Item.useAnimation = 36;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4.5f;
+            Item.value = Item.sellPrice(0, 0, 35, 0);
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item18;
+            Item.autoReuse = true;
+            Item.noMelee = true;
+            Item.shoot = ModContent.ProjectileType <GoldChakramProjectileOverride>();
+            Item.shootSpeed = 13.5f;
+            Item.noUseGraphic = true;
+        }
+        public override int GetVoid(Player player)
+        {
+            return 7;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(1).AddIngredient(ItemID.GoldBar, 15).AddTile(TileID.Anvils).Register();
+        }
+    }
+}
---
+++ Projectiles/Rogue/Void/GoldChakramProjectileOverride.cs
@@ -0,0 +1,33 @@
+﻿using CalamityMod;
+using Microsoft.Xna.Framework.Graphics;
+using SOTS.Projectiles.Ores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using TheBereftSouls.Void;
+
+namespace TheBereftSouls.Projectiles.Rogue.Void
+{
+    public class GoldChakramProjectileOverride : GoldChakram
+    {
+        public override string Texture => "SOTS/Projectiles/Ores/GoldChakram";
+        public override void SetDefaults()
+        {
+            Projectile.height = 32;
+            Projectile.width = 32;
+            Projectile.penetrate = -1;
+            Projectile.DamageType = ModContent.GetInstance<VoidRogue>();
+            Projectile.tileCollide = true;
+            Projectile.timeLeft = 715;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.extraUpdates = 2;
+        }
+        
+    }
+}
