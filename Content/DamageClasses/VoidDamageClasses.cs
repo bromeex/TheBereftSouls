daa6424f41f57b98595d92142d9c0618c3ad4639
schja278 <schja278@student.otago.ac.nz>
2024-10-18 22:17:20 +1300

    reformatting void item structure

---
+++ Content/DamageClasses/VoidDamageClasses.cs
@@ -0,0 +1,55 @@
+using Terraria.ModLoader;
+using SOTS.Void;
+using CalamityMod;
+using ThoriumMod;
+
+namespace TheBereftSouls.Content.DamageClasses
+{
+    public class VoidRogue : DamageClass
+    {
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+        {
+            if (damageClass == DamageClass.Generic || damageClass == ModContent.GetInstance<VoidGeneric>() || damageClass == ModContent.GetInstance<RogueDamageClass>() || damageClass == Throwing)
+                return StatInheritanceData.Full;
+            return StatInheritanceData.None;
+        }
+        public override bool GetEffectInheritance(DamageClass damageClass)
+        {
+            if (damageClass == ModContent.GetInstance<RogueDamageClass>() || damageClass == ModContent.GetInstance<VoidGeneric>() || damageClass == Throwing)
+                return true;
+            return false;
+        }
+    }
+
+    public class VoidBard : DamageClass
+    {
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+        {
+            if (damageClass == DamageClass.Generic || damageClass == ModContent.GetInstance<VoidGeneric>() || damageClass == ModContent.GetInstance<BardDamage>())
+                return StatInheritanceData.Full;
+            return StatInheritanceData.None;
+        }
+        public override bool GetEffectInheritance(DamageClass damageClass)
+        {
+            if (damageClass == ModContent.GetInstance<BardDamage>() || damageClass == ModContent.GetInstance<VoidGeneric>())
+                return true;
+            return false;
+        }
+    }
+
+    public class VoidHealer : DamageClass
+    {
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass)
+        {
+            if (damageClass == DamageClass.Generic || damageClass == ModContent.GetInstance<VoidGeneric>() || damageClass == ModContent.GetInstance<HealerDamage>())
+                return StatInheritanceData.Full;
+            return StatInheritanceData.None;
+        }
+        public override bool GetEffectInheritance(DamageClass damageClass)
+        {
+            if (damageClass == ModContent.GetInstance<HealerDamage>() || damageClass == ModContent.GetInstance<VoidGeneric>())
+                return true;
+            return false;
+        }
+    }
+}
