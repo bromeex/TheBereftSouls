c34e17943c58db15ea4439c24101d240767820cc
schja278 <schja278@student.otago.ac.nz>
2024-10-29 03:34:08 +1300

    Revert "Delete Common/Systems/KeyBindSystem.cs"
    This reverts commit 00193cb786620078bcfd488b9671f0be691858e2.

---
+++ Common/Systems/KeyBindSystem.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.ModLoader;
+
+namespace TheBereftSouls.Common.Systems
+{
+    public class KeybindSystem : ModSystem
+    {
+        public static ModKeybind VesperaEnchStone;
+
+        public override void Load()
+        {
+            VesperaEnchStone = KeybindLoader.RegisterKeybind(Mod, "Vespera Enchantment Stone", "U");
+        }
+
+        public override void Unload()
+        {
+            VesperaEnchStone = null;
+        }
+    }
+}
