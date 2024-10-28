16df15ef93827c24d397ee12258cb88c848d2290
schja278 <schja278@student.otago.ac.nz>
2024-10-29 03:01:02 +1300

    implemented weak refrence dependancy)

---
+++ Content/Items/Accessories/WormwoodEnchantment.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using SOTS.Items.Nature;
+using SOTS.Projectiles.Nature;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using TheBereftSouls.Content.Buffs;
+using TheBereftSouls.Utils;
+
+namespace TheBereftSouls.Content.Items.Accessories
+{
+    [ExtendsFromMod("SOTS")]
+    public class WormwoodEnchantment : ModItem
+    {
+        public List<Projectile> Hooks = new List<Projectile>(hooksToSummon);
+
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(defenseBoost);
+
+        private static int defenseBoost = 1;
+        private static int hooksToSummon = 4;
+
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.ItemNoGravity[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 34;
+            Item.accessory = true;
+            Item.value = Item.sellPrice(0, 5, 0, 0);
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.statDefense += player.numMinions * defenseBoost;
+            if (Main.myPlayer == player.whoAmI)
+            {
+                if (!player.HasBuff(ModContent.BuffType<PatchedUpDebuff>()) && player.statLife > player.statLifeMax2 / 2)
+                {
+                    if (Hooks.Count < hooksToSummon)
+                    {
+                        Hooks.Add(Projectile.NewProjectileDirect(player.GetSource_FromThis(), player.MountedCenter, Vector2.Zero, ModContent.ProjectileType<BloomingHook>(), 11, 1));
+                    }
+                }
+
+                foreach (var hook in Hooks)
+                {
+                    hook.timeLeft = 6;
+                }
+
+                if (player.statLife < player.statLifeMax / 2)
+                {
+                    foreach (var hook in Hooks)
+                    {
+                        hook.Kill();
+                        player.Heal(player.statLifeMax2 / 16);
+                        player.AddBuff(ModContent.BuffType<PatchedUpDebuff>(), 3600);
+                        BereftUtils.DustCircle(player.Center, 16, 10, DustID.GemEmerald, Main.rand.NextFloat(1f, 2f));
+                    }
+
+                    Hooks.Clear();
+                }
+
+                if (Hooks.Count > 0 && !Hooks[0].active)
+                {
+                    Hooks.Clear();
+                }
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<NatureWreath>());
+            recipe.AddIngredient(ModContent.ItemType<NatureShirt>());
+            recipe.AddIngredient(ModContent.ItemType<NatureLeggings>());
+            recipe.AddIngredient(ModContent.ItemType<BotanicalSymbiote>());
+            recipe.AddIngredient(ModContent.ItemType<NatureSpell>());
+            recipe.AddIngredient(ItemID.EmeraldStaff);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Friendly/VesperaStone.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using SOTS.Projectiles.Earth;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheBereftSouls.Content.Tiles.Special;
+using TheBereftSouls.Players;
+using TheBereftSouls.Utils;
+
+namespace TheBereftSouls.Content.Projectiles.Friendly
+{
+    [ExtendsFromMod("SOTS")]
+    public class VesperaStone : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.aiStyle = 0;
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.timeLeft = 30;
+            Projectile.alpha = 100;
+        }
+
+        public override void AI()
+        {
+            Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.PureSpray);
+            Projectile.rotation += MathHelper.ToRadians(Projectile.timeLeft);
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (Main.myPlayer == Projectile.owner)
+            {
+                for (int i = -1; i < 1; i++)
+                {
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedBy(MathHelper.ToRadians(i * 36)).SafeNormalize(Vector2.Zero) * 5, ModContent.ProjectileType<BigEvostonePebble>(), 15, 0.1f);
+                }
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            PlaceStone();
+            return true;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            if (timeLeft <= 1)
+            {
+                PlaceStone();
+            }
+        }
+
+        private void PlaceStone()
+        {
+            if (Main.myPlayer == Projectile.owner)
+            {
+                Point point = Projectile.position.ToTileCoordinates();
+                WorldGen.PlaceTile(point.X, point.Y, ModContent.TileType<VesperaStoneBlock>());
+                NetMessage.SendTileSquare(-1, point.X, point.Y);
+                BereftUtils.DustCircle(Main.player[Projectile.owner].Center, 16, 10, DustID.PureSpray);
+                Main.player[Projectile.owner].GetModPlayer<BereftSOTSPlayer>().VesperaStoneCoords.Add(new Vector2(point.X, point.Y));
+            }
+        }
+    }
+}
