8a7d2b3e868096ffaf9f1e5711a8a299121cec13
Raymond <rjmichalski2006@gmail.com>
2024-11-21 02:55:45 -0500

    Using Readonly Arrays
    Switched to Arrays instead of a list. Also, made them readonly and there own thing to make it better to edit later on.

--- Content/ItemChanges/CalamityStarterBagChanges.cs
+++ Content/ItemChanges/CalamityStarterBagChanges.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.GameContent.ItemDropRules;
@@ -5,19 +6,34 @@ using Terraria.ID;
 using CalamityMod.Items.TreasureBags.MiscGrabBags;
 using FargowiltasSouls.Content.Items.Accessories.Souls;
 
-namespace TheBereftSouls.Content.ItemChanges
+namespace TheBereftSouls.Content.ItemChanges;
+
+[ExtendsFromMod("CalamityMod", "FargowiltasSouls")]
+public class CalamityStarterBagChanges : GlobalItem
 {
-    [ExtendsFromMod("CalamityMod", "FargowiltasSouls")]
-    public class CalamityStarterBagChanges : GlobalItem
+    private static readonly IItemDropRule[] ItemsToAdd =
+    [
+        ItemDropRule.Common(ItemID.Zenith, 1, 1, 1),
+        ItemDropRule.Common(ModContent.ItemType<EternitySoul>(), 1, 1, 1),
+    ];
+
+    private static readonly IItemDropRule[] ItemsToRemove = [];
+    
+    public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
     {
-        public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
+        if (item.ModItem is not StarterBag)
         {
-            if (item.ModItem is StarterBag)
-            {
-                // Normal Item & Modded Item Example
-                // itemLoot.Add(ItemDropRule.Common(ItemID.Zenith, 1, 1, 1));
-                // itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<EternitySoul>(), 1, 1, 1));
-            }
+            return;
+        }
+
+        foreach (var dropRule in ItemsToAdd)
+        {
+            itemLoot.Add(dropRule);
+        }
+
+        foreach (var dropRule in ItemsToRemove)
+        {
+            itemLoot.Remove(dropRule);
         }
     }
 }
