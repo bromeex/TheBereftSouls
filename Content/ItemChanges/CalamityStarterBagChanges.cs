13cb77e908f5d86762a781db75f8d724d8ee4ead
Raymond <rjmichalski2006@gmail.com>
2024-11-21 20:00:26 -0500

    Bard & Healer Starter Items
    The starter bag now contains items for the bard and healer

--- Content/ItemChanges/CalamityStarterBagChanges.cs
+++ Content/ItemChanges/CalamityStarterBagChanges.cs
@@ -5,16 +5,18 @@ using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using CalamityMod.Items.TreasureBags.MiscGrabBags;
 using FargowiltasSouls.Content.Items.Accessories.Souls;
+using ThoriumMod.Items.BardItems;
+using ThoriumMod.Items.HealerItems;
 
 namespace TheBereftSouls.Content.ItemChanges;
 
-[ExtendsFromMod("CalamityMod", "FargowiltasSouls")]
+[ExtendsFromMod("CalamityMod", "FargowiltasSouls", "ThoriumMod")]
 public class CalamityStarterBagChanges : GlobalItem
 {
     private static readonly IItemDropRule[] ItemsToAdd =
     [
-        ItemDropRule.Common(ItemID.Zenith, 1, 1, 1),
-        ItemDropRule.Common(ModContent.ItemType<EternitySoul>(), 1, 1, 1),
+        ItemDropRule.Common(ModContent.ItemType<Pill>(), 1, 500, 500),
+        ItemDropRule.Common(ModContent.ItemType<Ukulele>(), 1, 1, 1),
     ];
 
     private static readonly IItemDropRule[] ItemsToRemove = [];
