09f22076717d50455f0a7abaa146c5457e5a9b5b
1K2S <171241044+1Git2Clone@users.noreply.github.com>
2024-11-23 20:31:17 +0200

    Removed unnecessary using directive

--- Content/RecipeChanges/ShimmerRecipeChanges.cs
+++ Content/RecipeChanges/ShimmerRecipeChanges.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 using ThoriumMod.Items.BardItems;
 using ThoriumMod.Items.Consumable;
