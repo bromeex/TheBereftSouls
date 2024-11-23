a67fb09ddb12f250c682a381e2b8ef4907413338
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-11-23 19:56:45 +0200

    Made the namespace file scoped

--- Content/RecipeChanges/FrigidPickaxe.cs
+++ Content/RecipeChanges/FrigidPickaxe.cs
@@ -2,29 +2,27 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace TheBereftSouls.Content.RecipeChanges
+namespace TheBereftSouls.Content.RecipeChanges;
+
+[ExtendsFromMod("SOTS")]
+public class FrigidPickaxe : ModSystem
 {
-    [ExtendsFromMod("SOTS")]
-    public class FrigidPickaxe : ModSystem
+    public override void PostAddRecipes()
     {
-        public override void PostAddRecipes()
+        for (int i = 0; i < Recipe.numRecipes; i++)
         {
-            for (int i = 0; i < Recipe.numRecipes; i++)
-            {
-                Recipe recipe = Main.recipe[i];
+            Recipe recipe = Main.recipe[i];
 
-                if (recipe.createItem.type != ModContent.ItemType<SOTS.Items.Tools.FrigidPickaxe>())
-                    continue;
+            if (recipe.createItem.type != ModContent.ItemType<SOTS.Items.Tools.FrigidPickaxe>())
+                continue;
 
-                Recipe alternativeRecipe = recipe.Clone();
+            Recipe alternativeRecipe = recipe.Clone();
 
-                recipe.AddIngredient(ItemID.TissueSample, 6);
+            recipe.AddIngredient(ItemID.TissueSample, 6);
 
-                alternativeRecipe.AddIngredient(ItemID.ShadowScale, 6);
-                alternativeRecipe.Register();
-                break;
-            }
+            alternativeRecipe.AddIngredient(ItemID.ShadowScale, 6);
+            alternativeRecipe.Register();
+            break;
         }
     }
 }
-
